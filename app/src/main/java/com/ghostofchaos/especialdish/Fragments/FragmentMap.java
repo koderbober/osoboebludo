da29df0abf4c37c8877548853425041b894b13ea
Ghost <p.gosteev@gmail.com>
2016-04-06 21:19:56 +0300

    updated marker

--- app/src/main/java/com/ghostofchaos/especialdish/Fragments/FragmentMap.java
+++ app/src/main/java/com/ghostofchaos/especialdish/Fragments/FragmentMap.java
@@ -1,25 +1,20 @@
 package com.ghostofchaos.especialdish.Fragments;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.ghostofchaos.especialdish.R;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Ghost on 30.03.2016.
@@ -37,30 +32,53 @@ public class FragmentMap extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_map, container, false);
         mapView = (MapView) root.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
         // Gets to GoogleMap from the MapView and does initialization stuff
         map = mapView.getMap();
-        map.getUiSettings().setMyLocationButtonEnabled(false);
-        map.setMyLocationEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
 
         // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
         MapsInitializer.initialize(this.getActivity());
 
         // Updates the location and zoom of the MapView
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
-        map.animateCamera(cameraUpdate);
+        map.setMyLocationEnabled(true);
+        Location myLocation = map.getMyLocation();
+
         if (map != null) {
+            map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                @Override
+                public View getInfoWindow(Marker marker) {
+                    LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.marker_info, null);
+                    View view = linearLayout.findViewById(R.id.info);
+                    return linearLayout;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    return null;
+                }
+            });
+/*
             Marker hamburg = map.addMarker(new MarkerOptions()
                     .position(HAMBURG)
                     .title("Hamburg"));
             Marker kiel = map.addMarker(new MarkerOptions()
                     .position(KIEL)
                     .title("Kiel")
-                    .snippet("Kiel is cool"));
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+                    .snippet("Kiel is cool"));*/
+
+            if (myLocation != null) {
+                double dLatitude = myLocation.getLatitude();
+                double dLongitude = myLocation.getLongitude();
+
+                // Move the camera instantly to hamburg with a zoom of 15.
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(dLatitude, dLongitude), 15));
+            }
+            // Zoom in, animating the camera.
+            map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
         }
         return root;
     }
--- app/src/main/java/com/ghostofchaos/especialdish/Fragments/FragmentSearch.java
+++ app/src/main/java/com/ghostofchaos/especialdish/Fragments/FragmentSearch.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -16,25 +19,54 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.ghostofchaos.especialdish.Adapter.SearchRestaurantsListAdapter;
 import com.ghostofchaos.especialdish.FontCache;
 import com.ghostofchaos.especialdish.Adapter.TabAdapter;
+import com.ghostofchaos.especialdish.Objects.RestaurantsModel;
 import com.ghostofchaos.especialdish.R;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 /**
  * Created by Ghost on 25.02.2016.
  */
 public class FragmentSearch extends Fragment {
 
+    ArrayList<RestaurantsModel> restaurantsModelList;
+    SearchRestaurantsListAdapter searchRestaurantsListAdapter;
+    ArrayList<RestaurantsModel> list;
+    ListView listView;
+    SwipeRefreshLayout swipeRefreshLayout;
     ProgressBar progressBar;
     Typeface typeface;
     FrameLayout frameLayout;
     View shadow;
     boolean refresh;
     int page;
+    String host;
+    View footer;
+
     private boolean loading = true;
+    private StringRequest stringRequest;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -58,7 +90,7 @@ public class FragmentSearch extends Fragment {
 
         for (int i = 0; i < tabAdapter.getCount(); i++) {
             TextView t = new TextView(getContext());
-            t.setText(tabAdapter.getPageTitle(i) );
+            t.setText(tabAdapter.getPageTitle(i));
             t.setTypeface(typeface);
             t.setTextColor(Color.parseColor("#FFFFFF"));
             t.setAllCaps(true);
@@ -100,10 +132,23 @@ public class FragmentSearch extends Fragment {
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
         menu.clear();
+        MenuInflater menuInflater = getActivity().getMenuInflater();
+        menuInflater.inflate(R.menu.menu_search, menu);
 
-        MenuItem search = menu.add("Search");
-        search.setIcon(R.drawable.vector_search);
-        search.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) search.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                getObjects(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         MenuItem tune = menu.add("Tune");
         tune.setIcon(R.drawable.vector_tune);
@@ -111,6 +156,69 @@ public class FragmentSearch extends Fragment {
 
         super.onPrepareOptionsMenu(menu);
     }
+
+    private void getObjects(String query) {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        try {
+            query = URLEncoder.encode(query, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        stringRequest = new StringRequest(Request.Method.GET, host + page, //+ "&keywords=" + query,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        if (refresh) {
+                            restaurantsModelList.clear();
+                        }
+                        if (s.equals("")) {
+                            listView.removeFooterView(footer);
+                            loading = false;
+                        } else {
+                            Log.d("stringRequest", s);
+                            Log.d("address", host + page);
+                            setList(s);
+                            restaurantsModelList.addAll(list);
+                            Log.d("list", restaurantsModelList.size() + "");
+                            searchRestaurantsListAdapter.notifyDataSetChanged();
+                            Log.d("listadapter", searchRestaurantsListAdapter.getCount() + "");
+                            swipeRefreshLayout.setRefreshing(false);
+                            swipeRefreshLayout.setEnabled(false);
+                            loading = true;
+                            page++;
+                            progressBar.setVisibility(View.GONE);
+                            footer.setVisibility(View.GONE);
+                            refresh = false;
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.d("news", volleyError.toString());
+                        Toast.makeText(getActivity(), "При загрузке данных произошла ошибка", Toast.LENGTH_SHORT).show();
+                        swipeRefreshLayout.setRefreshing(false);
+                        swipeRefreshLayout.setEnabled(false);
+                        loading = true;
+                    }
+                }) {
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                10000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(stringRequest);
+    }
+
+    public void setList(String s) {
+        Gson gson = new Gson();
+        JsonParser parser = new JsonParser();
+        JsonArray jArray = parser.parse(s).getAsJsonArray();
+        Type type = new TypeToken<ArrayList<RestaurantsModel>>() {
+        }.getType();
+        list = gson.fromJson(jArray, type);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
